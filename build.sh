75540eb7b42551fa0fa3f946eccdca880a90c411
blackdogjet@protonmail.com <blackdogjet@protonmail.com>
2026-01-06 21:01:10 -0600

    D: Changed repo name.

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-python3 src/main.py "https://github.com/BlackDogJet/StaticSiteGenerator"
+python3 src/main.py "https://github.com/BlackDogJet/build-static-site-generator-python/"
--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ template_path = "./template.html"
 def main():
     base_path = "./"
     if sys.argv[0]:
-        base_path = sys.argv[1] + "/"
+        base_path = sys.argv[1]
     
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
